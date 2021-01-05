af2304f9550e8a6a1fb936bac88f960a9277c6a4
Anthony J. Perez <AnthonyJPerez24@gmail.com>
2021-01-05 01:05:17 -0800

    Refined commandline params

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo ./compile_xmr-stak-rx.sh
 # optimize the installation
 sudo sysctl -w vm.nr_hugepages=1300
 # run a miner
-./xmr-stak-rx --noAMD --noNVIDIA --noTest
+./xmr-stak-rx --noAMD --noNVIDIA --noTest --url pool.supportxmr.com:5555 --user 48Vm2HV9KbH6RadujKH7dDVPGomWASA7u8KjpN1bvcBsE6cJPCSognaCFoZmJHYA6PQMr7LCaWZF5Vzd2TagBGVVNcL8muN --currency monero
