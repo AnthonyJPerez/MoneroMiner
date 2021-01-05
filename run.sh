8a146314cc0ae0c191faad01a16d4507bb5bd6e7
Anthony J. Perez <AnthonyJPerez24@gmail.com>
2021-01-04 23:54:59 -0800

    randomx_booster.sh doesn't exist, so just set huge page support manually

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# compile xmr-stak-rx
 sudo ./compile_xmr-stak-rx.sh
-sudo apt install wrmsr numactl
-sudo ./randomx_booster.sh
+# optimize the installation
+sudo sysctl -w vm.nr_hugepages=1300
+# run a miner
 ./xmr-stak-rx --noAMD --noNVIDIA --noTest
