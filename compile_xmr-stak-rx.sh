fe52f9520c7e16ab45df2cead8c2594b973681c0
Anthony J. Perez <AnthonyJPerez24@gmail.com>
2021-05-16 17:43:22 -0700

    fixed commands

--- compile_xmr-stak-rx.sh
+++ compile_xmr-stak-rx.sh
@@ -1,5 +1,4 @@
-sudo apt-get install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev
-#sudo apt-get install -y build-essential libssl-dev cmake ocl-icd-opencl-dev
+sudo apt-get -y update && sudo apt-get install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ocl-icd-opencl-dev
 git clone https://github.com/fireice-uk/xmr-stak.git -b xmr-stak-rx
 chmod -R u=rwx,go=rwx ./xmr-stak
 mkdir xmr-stak/build
